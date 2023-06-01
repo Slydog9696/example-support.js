610af7904f710650c0c23ff103b99cf17ca75a16
git-cameronbryce <developmentcameronbryce@gmail.com>
2023-05-31 19:35:25 -0600

    Finished onReaction event, working and tested.

--- src/event-handler/event-onReact.js
+++ src/event-handler/event-onReact.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, EmbedBuilder } = require('discord.js');
 
 process.on("unhandledRejection", (err) => console.error(err));
 
@@ -14,18 +14,32 @@ module.exports = {
       if (interaction.customId === 'verified-stego-baby') {
 
         const giveRole = async () => {
-          await interaction.member.roles.add(verifiedRole)
+
+          const embed = new EmbedBuilder()
+            .setDescription(`Discord Role: <@&${verifiedRole}>\nHas been added, successfully.`)
+            .setFooter({ text: 'Tip: Contact support if there are issues.' })
+            .setColor('#2ecc71')
+
           console.log('Role Given')
+          await interaction.member.roles.add(verifiedRole)
+          await interaction.followUp({ embeds: [embed], ephemeral: true })
         }
 
         const removeRole = async () => {
-          await interaction.member.roles.add(verifiedRole)
-          console.log('Role Taken')
+
+          const embed = new EmbedBuilder()
+            .setDescription(`Discord Role: <@&${verifiedRole}>\nHas been removed, successfully.`)
+            .setFooter({ text: 'Tip: Contact support if there are issues.' })
+            .setColor('#2ecc71')
+
+          console.log('Role Removed')
+          await interaction.member.roles.remove(verifiedRole)
+          await interaction.followUp({ embeds: [embed], ephemeral: true })
         }
 
         const verifiedRole = '1049521297174040689';
         interaction.member.roles.cache.has(verifiedRole)
-          ? giveRole() : removeRole()
+          ? removeRole() : giveRole()
       }
 
     });
